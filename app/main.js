6c9173af5b54431fcc4707ea0c41a61a500664cd
nilampadval <nilam.padval@citiustech.com>
2017-09-26 10:42:57 +0530

    minified file used

--- app/main.js
+++ app/main.js
@@ -1,13 +1,13 @@
 
 require.config({
 	paths: {
-		angular: '../../bower_components/angular/angular',
+		angular: '../../bower_components/angular/angular.min',
 		angularRoute: '../../bower_components/angular-ui-router/release/angular-ui-router.min',
 		angularMaterial:'../../bower_components/angular-material/angular-material.min',
 		angularAnimate:'../../bower_components/angular-animate/angular-animate.min',
 		angularAria:'../../bower_components/angular-aria/angular-aria.min',
-		ngFileUpload:'../../bower_components/ng-file-upload/ng-file-upload',
-		ngFileUploadShim:'../../bower_components/ng-file-upload/ng-file-upload-shim',
+		ngFileUpload:'../../bower_components/ng-file-upload/ng-file-upload.min',
+		ngFileUploadShim:'../../bower_components/ng-file-upload/ng-file-upload-shim.min',
 		materialJS:'js/material.min'
 	},
 	shim: {
